dd32e6155f78297631f41633c323c1f3fd0e481b
ThomasWecker <weckerth@mediamarktsaturn.com>
2019-05-05 18:37:04 +0200

    initial commit - working state

---
+++ myGetPurchaseOrderSap/index.js
@@ -0,0 +1,231 @@
+'use strict';
+
+/**
+ * This is a demo showcasing a simple sap conversational ui build with the Amazon Alexa Skills Kit.
+ */
+
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to the MediaMarktSaturn Conversational SAP Demo with Alexa. ' +
+        'How can I help you?';
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Please ask me a question by saying, ' +
+        'What is the delivery date for 4502830392';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for using MediaMarktSaturn Conversational SAP. Have a nice day!';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function setDeliveryIntentSesion(intent, session, callback) {
+    let sessionAttributes = {};
+    const cardTitle = intent.name;
+    let speechOutput = '';
+    let repromptText = '';
+    const shouldEndSession = false;
+
+    // Get the Utterance variables
+    const myPurchaseOrderSlot = intent.slots.PurchaseOrder;
+    console.log("==> Called Intent:" + cardTitle + "Slot:" + myPurchaseOrderSlot);
+
+    if (myPurchaseOrderSlot) {
+
+        const myPurchaseOrder = myPurchaseOrderSlot.value;
+
+        speechOutput = `Here are your informations for Purchase Order ${myPurchaseOrder}.`;
+        repromptText = null;
+
+        var http = require('http');
+
+        const hostname = "http://35.189.196.22:8000/";
+        var path = "sap/opu/odata/sap/ZCC_GETPURCHASEORDERDATA_CDS/";
+        var entitySet = "ZCC_GetPurchaseOrderData";
+        var filter = "/?$filter=PurchaseOrder eq '";
+        var format = "'&$format=json"; //
+        var uri = hostname + path + entitySet + filter + myPurchaseOrder + format;
+
+        console.log("Get data from: " + uri);
+
+        http.get(uri, function(response) {
+            console.log(uri);
+            console.log('GET status: ' + response.statusCode);
+
+            var rawData = '';
+
+            response.on('data', function(chunck) {
+                console.log('BODY: ' + chunck);
+                console.log(chunck);
+                rawData += chunck;
+                var parsedData = JSON.parse(rawData);
+                var myData = parsedData.d.results;
+
+                var myDeliveryDate;
+
+                myData.forEach(function(myData){
+                    var jsonDate = (myData.DeliveryDate);
+                    function parseJsonDate(jsonDateString){
+                        return new Date(parseInt(jsonDateString.replace('/Date(', '')));
+                    }
+                    myDeliveryDate = parseJsonDate(jsonDate);
+                });
+
+                console.log(myDeliveryDate);
+
+                speechOutput = `Here are your informations for Purchase Order ${myPurchaseOrder}.` +
+                    `This Purchase Order will get delivered on ${myDeliveryDate}.`;
+                callback(sessionAttributes,
+                    buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+
+            });
+
+        });
+
+
+    } else {
+        speechOutput = "Sorry I'm not speaking bavarian. Please try again.";
+        repromptText = null;
+        callback(sessionAttributes,
+            buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+    }
+}
+
+
+// --------------- Events -----------------------
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'DeliveryIntent') {
+        setDeliveryIntentSesion(intent, session, callback);
+    } else if (intentName === 'AMAZON.HelpIntent') {
+        getWelcomeResponse(callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
+
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+             callback('Invalid Application ID');
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
